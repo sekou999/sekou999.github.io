2d2951308935c233b6e915b21baf7feb4b10b4e1
sekou999 <winblack38@gmail.com>
2024-07-07 15:26:47 +0000

    Modification de mon depot

--- fiveserver.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    php: "C:/wamp64/bin/php/php8.3.6"
-};
-
- // Chemin vers PHP sur Windows
-// php: "/usr/bin/php" // Chemin vers PHP sur macOS/Ubuntu
--- message_connec.js
+++
@@ -1,6 +0,0 @@
-const messageConnexion = document.getElementById(message_A);
-const conteneur = document.querySelector(".conteneur");
-
-conteneur.style.visibility="visible";
-conteneur.style.backGroundColor="red";
-conteneur.style.backGroundColor="green";
